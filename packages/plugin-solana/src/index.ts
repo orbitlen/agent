215bbd831e17b06f6317d2548c896067a22b8de8
cxp-13 <lantianlaoli@gmail.com>
2025-01-10 15:02:09 +0800

    feat: add dotenv dependency and implement deposit and fetch wallet actions in Solana plugin

--- packages/plugin-solana/src/index.ts
+++ packages/plugin-solana/src/index.ts
@@ -3,24 +3,22 @@ export * from "./providers/wallet.ts";
 
 import { Plugin } from "@elizaos/core";
 import { executeBorrow } from "./actions/borrow.ts";
+import { executeDeposit } from "./actions/deposit.ts";
+import { executeFetchWalletAndOAInfo } from "./actions/fetch_wallet_or_oa_info.ts";
 
 import { walletProvider } from "./providers/wallet.ts";
 import { TokenProvider } from "./providers/token.ts";
 import { WalletProvider } from "./providers/wallet.ts";
 import { anchorFrameworkProvider } from "./providers/anchor.ts";
 
-
 export { TokenProvider, WalletProvider };
 
-
 export const solanaPlugin: Plugin = {
-    name: "solana",
-    description: "Solana Plugin for Eliza",
-    actions: [
-        executeBorrow,
-    ],
-    evaluators: [],
-    providers: [anchorFrameworkProvider],
+  name: "solana",
+  description: "Solana Plugin for Eliza",
+  actions: [executeBorrow, executeDeposit, executeFetchWalletAndOAInfo],
+  evaluators: [],
+  providers: [anchorFrameworkProvider],
 };
 
 export default solanaPlugin;
