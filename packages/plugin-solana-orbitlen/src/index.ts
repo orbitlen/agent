94cc750aabd3934457610a0898e6b075bfc3fa50
cxp-13 <lantianlaoli@gmail.com>
2025-01-12 11:11:53 +0800

    feat: implement raydium part

--- packages/plugin-solana-orbitlen/src/index.ts
+++ packages/plugin-solana-orbitlen/src/index.ts
@@ -4,6 +4,12 @@ import { Plugin } from "@elizaos/core";
 import { executeBorrow } from "./actions/borrow.ts";
 import { executeDeposit } from "./actions/deposit.ts";
 import { executeFetchWalletAndOAInfo } from "./actions/fetch_wallet_or_oa_info.ts";
+import { executeLiquidate } from "./actions/liquidate.ts";
+import { executeRaydiumDeposit } from "./actions/raydium/deposit.ts";
+import { executeRaydiumWithdraw } from "./actions/raydium/withdraw.ts";
+import { executeRaydiumSwap } from "./actions/raydium/swap.ts";
+
+
 
 import { walletProvider } from "./providers/wallet.ts";
 import { WalletProvider } from "./providers/wallet.ts";
@@ -14,7 +20,15 @@ export { WalletProvider };
 export const solanaPlugin: Plugin = {
   name: "solana",
   description: "Solana Plugin for Eliza",
-  actions: [executeBorrow, executeDeposit, executeFetchWalletAndOAInfo],
+  actions: [
+    executeBorrow,
+    executeDeposit,
+    executeFetchWalletAndOAInfo,
+    executeLiquidate,
+    executeRaydiumDeposit,
+    executeRaydiumWithdraw,
+    executeRaydiumSwap
+  ],
   evaluators: [],
   providers: [anchorFrameworkProvider],
 };
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import {
 } from "@elizaos/core";
 import { bootstrapPlugin } from "@elizaos/plugin-bootstrap";
 import { createNodePlugin } from "@elizaos/plugin-node";
-import { solanaPlugin } from "@elizaos/plugin-solana";
+import { solanaPlugin } from "@elizaos/plugin-solana-orbitlen";
 import fs from "fs";
 import net from "net";
 import path from "path";
